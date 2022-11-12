224688c61b312c1b11152b2969ce09c47612ca65
Sylvie Godard <sylviegodard@MBP-de-Sylvie.lan>
2022-11-12 12:33:12 +0100

    delete unused files, add header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,53 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>Accueil</li>
+          <li>Concerts</li>
+          <li>Festivals</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+    <p>C'est sylvie</p>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss">
+header {
+  nav {
+    background-color: cadetblue;
+    box-shadow: 1px 5px 5px grey;
+  }
+
+  ul {
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+    list-style: none;
+    padding: 2rem;
+    li {
+      color: white;
+      cursor: pointer;
+      font-size: 20px;
+      font-weight: bold;
+      transition: 0.3s;
+    }
+
+    li:hover {
+      color: lightblue;
+    }
+    li:not(:last-child):after {
+      content: "|";
+      margin-left: 2rem;
+    }
+  }
+}
+</style>
