4a714ebe824eb22778dea51514ae3dd23fdcc757
Sylvie Godard <sylviegodard@MacBook-Pro-de-Sylvie.local>
2022-12-12 23:09:58 +0100

    add search bar

---
+++ src/components/HeaderHome/HeaderHome.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>Accueil</li>
+        <li>Concerts</li>
+        <li>Festivals</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+header {
+  nav {
+    background-color: cadetblue;
+    box-shadow: 1px 5px 5px grey;
+    margin-bottom: 2rem;
+  }
+
+  ul {
+    display: flex;
+    gap: 2rem;
+    justify-content: center;
+    list-style: none;
+    padding: 2rem;
+    li {
+      color: white;
+      cursor: pointer;
+      font-size: 20px;
+      font-weight: bold;
+      transition: 0.3s;
+    }
+
+    li:hover {
+      color: lightblue;
+    }
+    li:not(:last-child):after {
+      content: "|";
+      margin-left: 2rem;
+    }
+  }
+}
+</style>
---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <input
+    type="text"
+    class="searchBar"
+    placeholder="Ceci est la barre recherche"
+  />
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
+
+<style lang="scss">
+input {
+  width: 26rem;
+  display: flex;
+  margin: 0 auto 2rem auto;
+}
+</style>
