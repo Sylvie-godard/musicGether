e5a1325abcdfeada0b16455c6bc0a7af408be397
Sylvie Godard <sylviegodard@MacBook-Pro-de-Sylvie.local>
2022-12-13 18:45:43 +0100

    refacto: slider, put in separate component

---
+++ src/components/SliderHomePage/SliderHomePage.vue
@@ -0,0 +1,111 @@
+<script>
+export default {
+  name: "SliderHomePage",
+  setup() {
+    const errorGap = 2;
+
+    /**
+     * click on button next to be on next slide (Carousel HomePage)
+     */
+    const next = () => {
+      const slidesContainer = document.getElementById("slider");
+      const slide = document.querySelector(".slide");
+      const slideWidth = slide.clientWidth + errorGap;
+      slidesContainer.scrollLeft += slideWidth;
+    };
+
+    /**
+     * click on button previous to be on previous slide (Carousel HomePage)
+     */
+    const previous = () => {
+      const slidesContainer = document.getElementById("slider");
+      const slide = document.querySelector(".slide");
+      const slideWidth = slide.clientWidth + errorGap;
+      console.log({
+        clientWidth: slidesContainer.clientWidth,
+        left: slide.clientWidth,
+      });
+      slidesContainer.scrollLeft -= slideWidth;
+    };
+
+    return {
+      next,
+      previous,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div id="slider">
+      <div class="slide"></div>
+      <div class="slide"></div>
+      <div class="slide"></div>
+    </div>
+    <div class="buttonCarousel">
+      <div class="dotButtonCarousel" @click="previous">&#8249;</div>
+      <div class="dotButtonCarousel" @click="next">&#8250;</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  margin: auto;
+  width: 100%;
+}
+
+.buttonCarousel {
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  justify-content: space-between;
+  height: 100%;
+  align-items: center;
+
+  .dotButtonCarousel {
+    cursor: pointer;
+    border-radius: 50%;
+    width: 50px;
+    font-size: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+  }
+}
+
+#slider {
+  width: 100%;
+  display: flex;
+  height: 20rem;
+  margin: auto;
+  overflow: hidden;
+  scroll-behavior: smooth;
+
+  .slide {
+    color: white;
+    text-align: center;
+    flex: 1 0 100%;
+  }
+  .slide:nth-child(1) {
+    background-color: pink;
+    height: 100%;
+    padding: 2rem;
+  }
+
+  .slide:nth-child(2) {
+    background-color: #804852;
+    height: 100%;
+    padding: 2rem;
+  }
+
+  .slide:nth-child(3) {
+    background-color: #503e41;
+    height: 100%;
+    padding: 2rem;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,10 @@
 <script>
 import HeaderHome from "@/components/HeaderHome/HeaderHome.vue";
 import SearchBar from "@/components/SearchBar/SearchBar.vue";
+import SliderHomePage from "@/components/SliderHomePage/SliderHomePage.vue";
 
 export default {
-  components: { SearchBar, HeaderHome },
-  setup() {
-    const next = () => {
-      const slidesContainer = document.getElementById("slider");
-      const slide = document.querySelector(".slide");
-      const slideWidth = slide.clientWidth;
-      slidesContainer.scrollLeft += slideWidth;
-    };
-
-    const previous = () => {
-      const slidesContainer = document.getElementById("slider");
-      const slide = document.querySelector(".slide");
-      const slideWidth = slide.clientWidth;
-      slidesContainer.scrollLeft -= slideWidth;
-    };
-
-    return {
-      next,
-      previous,
-    };
-  },
+  components: { SliderHomePage, SearchBar, HeaderHome },
 };
 </script>
 
@@ -31,62 +12,8 @@ export default {
   <div>
     <HeaderHome />
     <SearchBar />
-    <div class="container">
-      <div id="slider">
-        <div class="slide"></div>
-        <div class="slide"></div>
-        <div class="slide"></div>
-      </div>
-      <div class="buttonCarousel">
-        <button id="1" @click="previous">previous</button>
-        <button id="2" @click="next">next</button>
-      </div>
-    </div>
+    <SliderHomePage />
   </div>
 </template>
 
-<style lang="scss">
-.container {
-  margin: auto;
-  width: 80%;
-}
-
-.buttonCarousel {
-  display: flex;
-}
-
-#slider {
-  width: 100%;
-  border: 1px solid rebeccapurple;
-  display: flex;
-  height: 10rem;
-  margin: auto;
-  overflow: hidden;
-  scroll-behavior: smooth;
-
-  .slide {
-    border: 1px solid;
-    color: white;
-    text-align: center;
-    width: 100%;
-    flex: 1 0 100%;
-  }
-  .slide:nth-child(1) {
-    background-color: pink;
-    height: 100%;
-    padding: 2rem;
-  }
-
-  .slide:nth-child(2) {
-    background-color: #804852;
-    height: 100%;
-    padding: 2rem;
-  }
-
-  .slide:nth-child(3) {
-    background-color: #503e41;
-    height: 100%;
-    padding: 2rem;
-  }
-}
-</style>
+<style lang="scss"></style>
