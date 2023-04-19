b0dcf6dac343be25ed3bf910300b06babf1a92ee
Sylvie Godard <sylviegodard@MacBook-Pro-de-Sylvie.local>
2023-04-19 16:12:38 +0200

    delete unusude media,

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,32 @@
 <script>
+import image from "/src/assets/images/gifPandaCours.gif";
+import { defineComponent } from "vue";
 
-export default {
+export default defineComponent({
   name: "HomeView",
   components: {},
-};
+  setup() {
+    return {
+      image,
+    };
+  },
+});
 </script>
 
 <template>
   <div class="titleWebsite">Mon petit planner</div>
   <div class="container">
-    <img src="" alt="">
-    Hello World
+    <img :src="image" alt="" />
+    <div class="budget">
+      <div class="containerBudget">
+        <p>Pour moi</p>
+        <div class="amount">255€</div>
+      </div>
+      <div class="containerBudget">
+        <p>De côté</p>
+        <div class="amount">3000€</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +38,26 @@ export default {
   box-shadow: 5px 5px 5px black;
   background-color: white;
   border-radius: 30px;
+
+  .amount {
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px black;
+    padding: 1rem;
+  }
+
+  .budget {
+    display: flex;
+    width: 25rem;
+    align-items: end;
+    justify-content: space-between;
+    margin-left: auto;
+  }
+
+  .containerBudget {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
 
 .titleWebsite {
